ca3ed1fdbae315d577744ab4e62b84be1490cf4a
amitko92 <amitko92@gmail.com>
2025-08-29 12:00:08 +0300

    finish the quest https://leetcode.com/problems/palindrome-number/

---
+++ src/q_9_palindrome_umber/Solution.java
@@ -0,0 +1,26 @@
+package q_9_palindrome_umber;
+
+public class Solution {
+
+    public boolean isPalindrome(int x) {
+
+        String str = String.valueOf(x);
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
+
+// https://leetcode.com/problems/palindrome-number/
